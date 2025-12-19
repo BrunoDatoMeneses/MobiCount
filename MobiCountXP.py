4fb849141a309fc6d11f8b3b8fc59ac7dc24be51
bdato <bruno.dato@irit.fr>
2025-12-19 10:50:32 +0100

    fix: minor

--- MobiCountXP.py
+++ MobiCountXP.py
@@ -377,8 +377,9 @@ def count(VIDEO_NAME, START_DATE_AND_HOUR, REGION):
 
 
 
-VIDEO_LIST = ["1191553-hd_1920_1080_25fps",
-              "no-audio_1GX010072",
+VIDEO_LIST = [
+              #"1191553-hd_1920_1080_25fps",
+              #"no-audio_1GX010072",
               "no-audio_1GX020072",
               "no-audio_1GX030072",
               "no-audio_1GX040072",
@@ -393,8 +394,9 @@ VIDEO_LIST = ["1191553-hd_1920_1080_25fps",
 START_DATE_AND_TIME = datetime(2025, 11, 25, 9, 32, 9)
 MAX_DURATION = timedelta(minutes=8, seconds=51)
 
-DATES_LIST = [datetime(2025, 1, 1, 14, 32, 9),
-              START_DATE_AND_TIME,
+DATES_LIST = [
+              #datetime(2025, 1, 1, 14, 32, 9),
+              #START_DATE_AND_TIME,
               START_DATE_AND_TIME+MAX_DURATION,
               START_DATE_AND_TIME+2*MAX_DURATION,
               START_DATE_AND_TIME+3*MAX_DURATION,
@@ -410,8 +412,9 @@ REGION_TEST = [(960, 0), (960, 1080)]  # VERTICAL LINE 1080
 REGION_1 = [(901, 0), (901, 2028)]  # VERTICAL LINE 2K first tier
 REGION_2 = [(1352, 0), (1352, 2028)]  # VERTICAL LINE 2K middle 
 
-REGION_LIST = [REGION_TEST,
-               REGION_1,
+REGION_LIST = [
+               #REGION_TEST,
+               #REGION_1,
                REGION_1,
                REGION_1,
                REGION_1,
