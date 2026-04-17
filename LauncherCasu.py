9d7802a94311e4095cb22df75d365b9ed82e6a01
bdato <bruno.dato@irit.fr>
2026-04-17 14:16:54 +0200

    fix: casu launcher

--- LauncherCasu.py
+++ LauncherCasu.py
@@ -1,7 +1,7 @@
 import subprocess
 
 subprocess.Popen(
-    ["python", "C:/Users/bdato/Documents/MobiCount/RunOnCasu.py"],
+    ["C:/Users/bdato/Documents/.venv/Scripts/python.exe", "C:/Users/bdato/Documents/MobiCount/RunOnCasu.py"],
     stdout=subprocess.DEVNULL,
     stderr=subprocess.DEVNULL,
     stdin=subprocess.DEVNULL,
