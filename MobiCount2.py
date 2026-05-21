ba7b70179977c5f13fe9ce4db3af280bc87133d0
bdato <bruno.dato@irit.fr>
2026-05-21 15:36:44 +0200

    minor changes

--- MobiCount2.py
+++ MobiCount2.py
@@ -696,6 +696,7 @@ def config(VIDEO_NAME, START_DATE_AND_HOUR, REGION, PROJECT_FOLDER, FFMPEG_PATH,
         blur_ratio=0.5,
         max_hist = 5,
         device = "cpu", # Specifies the device for inference (e.g., cpu, cuda:0 or 0). Allows users to select between CPU, a specific GPU, or other compute devices for model execution.
+        #device = "cuda:0", 
     )
 
 
