d294f98c650667954e7f92137dfa0b150485e729
bdato <bruno.dato@irit.fr>
2026-07-17 09:17:06 +0200

    fix: script config occidata

--- LauncherConfigCasu.py
+++ LauncherConfigCasu.py
@@ -1,7 +1,7 @@
 import subprocess
 
 subprocess.Popen(
-    ["python3", "RunConfigOnOccidata.py"],
+    ["python", "RunConfigOnOccidata.py"],
     stdout=subprocess.DEVNULL,
     stderr=subprocess.DEVNULL,
     stdin=subprocess.DEVNULL,
